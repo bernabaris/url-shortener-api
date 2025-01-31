3b4a8a2879ff580e5af04b234febd686072229af
bernabaris <bernabaris1 @gmail.com>
2025-01-31 12:17:28 +0300

    Initial commit

---
+++ src/test/java/com/github/bernabaris/urlshortenerapi/UrlShortenerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bernabaris.urlshortenerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
